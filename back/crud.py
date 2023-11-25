19d0c7f0e8e81faaabcaef80632a5ce3cbd88277
Abdelhamid salhi <essalhi12345@gmail.com>
2023-11-25 13:21:33 +0100

    impl update/delete endpoints

--- back/crud.py
+++ back/crud.py
@@ -11,3 +11,17 @@ def create_recipe(db: Session, recipe_data: dict):
 
 def get_recipes(db: Session, skip: int = 0, limit: int = 10):
     return db.query(Recipe).offset(skip).limit(limit).all()
+
+def get_recipe_by_id(db: Session, recipe_id: int):
+    return db.query(Recipe).filter(Recipe.id == recipe_id).first()
+
+def update_recipe(db: Session, existing_recipe: Recipe, recipe_data: dict):
+    for key, value in recipe_data.items():
+        setattr(existing_recipe, key, value)
+    db.commit()
+    db.refresh(existing_recipe)
+    return existing_recipe
+
+def delete_recipe(db: Session, recipe: Recipe):
+    db.delete(recipe)
+    db.commit()
--- back/main.py
+++ back/main.py
@@ -13,7 +13,7 @@ def get_db():
         yield db
     finally:
         db.close()
-    
+
 # Endpoint to create a recipe
 @app.post("/recipes/")
 def create_recipe(recipe_data: dict, db: Session = Depends(get_db)):
@@ -24,3 +24,23 @@ def create_recipe(recipe_data: dict, db: Session = Depends(get_db)):
 def read_recipes(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     recipes = crud.get_recipes(db, skip=skip, limit=limit)
     return recipes
+
+# Endpoint to update a recipe by ID
+@app.put("/recipes/{recipe_id}")
+def update_recipe(recipe_id: int, recipe_data: dict, db: Session = Depends(get_db)):
+    existing_recipe = crud.get_recipe_by_id(db, recipe_id)
+    if existing_recipe is None:
+        raise HTTPException(status_code=404, detail="Recipe not found")
+
+    updated_recipe = crud.update_recipe(db, existing_recipe, recipe_data)
+    return updated_recipe
+
+# Endpoint to delete a recipe by ID
+@app.delete("/recipes/{recipe_id}")
+def delete_recipe(recipe_id: int, db: Session = Depends(get_db)):
+    existing_recipe = crud.get_recipe_by_id(db, recipe_id)
+    if existing_recipe is None:
+        raise HTTPException(status_code=404, detail="Recipe not found")
+
+    crud.delete_recipe(db, existing_recipe)
+    return {"message": "Recipe deleted successfully"}
